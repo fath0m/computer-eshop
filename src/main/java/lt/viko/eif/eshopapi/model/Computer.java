12c72edde14810bf8ec7ee0cbdaf642494080cca
Jonas Ritchi <ritchijon@gmail.com>
2021-06-06 21:20:11 +0300

    Added initial models of computer and it's parts

--- src/main/java/lt/viko/eif/eshopapi/model/Computer.java
+++ src/main/java/lt/viko/eif/eshopapi/model/Computer.java
@@ -1,5 +1,8 @@
 package lt.viko.eif.eshopapi.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lt.viko.eif.eshopapi.constant.Currency;
+
 import javax.persistence.*;
 
 @Entity
@@ -9,5 +12,106 @@ public class Computer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
+    private double price;
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "manufacturer_id", nullable = false)
+    private Manufacturer manufacturer;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "processor_id", nullable = false)
+    private Processor processor;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "graphics_card_id", nullable = false)
+    private GraphicsCard graphicsCard;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "memory_id", nullable = false)
+    private Memory memory;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "storage_id", nullable = false)
+    private Storage storage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Processor getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(Processor processor) {
+        this.processor = processor;
+    }
+
+    public GraphicsCard getGraphicsCard() {
+        return graphicsCard;
+    }
+
+    public void setGraphicsCard(GraphicsCard graphicsCard) {
+        this.graphicsCard = graphicsCard;
+    }
+
+    public Memory getMemory() {
+        return memory;
+    }
+
+    public void setMemory(Memory memory) {
+        this.memory = memory;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+
+    public void setStorage(Storage storage) {
+        this.storage = storage;
+    }
 
 }
