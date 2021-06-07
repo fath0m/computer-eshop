d74c3dca733faa65f6784e7b202ee0459e0b6eb2
PC <lsongulija@gmail.com>
2021-06-07 17:47:51 +0200

    Computer contoller methods getComputers, getComputerById, deleteComputerById added

--- src/main/java/lt/viko/eif/eshopapi/controller/ComputerController.java
+++ src/main/java/lt/viko/eif/eshopapi/controller/ComputerController.java
@@ -1,22 +1,42 @@
 package lt.viko.eif.eshopapi.controller;
 
 import lt.viko.eif.eshopapi.model.Computer;
+import lt.viko.eif.eshopapi.repository.ComputerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/computers", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ComputerController {
+
+    @Autowired
+    ComputerRepository computerRepository;
 
     /**
      * Get a list of computers
      * @return
      */
     @GetMapping
-    public String getComputers() {
-        return "Computers";
+    public ResponseEntity<CollectionModel<Computer>> getComputers() {
+        CollectionModel<Computer> model = CollectionModel.of(computerRepository.findAll());
+
+        final String uriString = ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString();
+        model.add(Link.of(uriString, "self"));
+        model.add(linkTo(methodOn(ComputerController.class).getComputerById(12L)).withRel("get-one-by-id"));
+
+        return ResponseEntity.ok(model);
     }
 
     /**
@@ -25,8 +45,20 @@ public class ComputerController {
      * @return
      */
     @GetMapping("/{id}")
-    public String getComputerById(@PathVariable(value = "id") long id){
-        return "Computer by id";
+    public ResponseEntity<EntityModel<Computer>> getComputerById(@PathVariable(value = "id") long id){
+        Optional<Computer> computer = computerRepository.findById(id);
+
+        if(computer.isEmpty())
+        {
+            return ResponseEntity.notFound().build();
+        }
+
+        EntityModel<Computer> model = EntityModel.of(computer.get());
+        final String uriString = ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString();
+        model.add(Link.of(uriString, "self"));
+        model.add(linkTo(methodOn(ComputerController.class).getComputers()).withRel("get-all"));
+
+        return ResponseEntity.ok(model);
     }
 
     /**
@@ -35,8 +67,15 @@ public class ComputerController {
      * @return
      */
     @DeleteMapping("/{id}")
-    public String deleteComputerById(@PathVariable long id){
-        return "Delete by id";
+    public ResponseEntity deleteComputerById(@PathVariable long id){
+        Optional<Computer> computer = computerRepository.findById(id);
+
+        if (computer.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        computerRepository.delete(computer.get());
+        return ResponseEntity.ok().build();
     }
 
     /**
