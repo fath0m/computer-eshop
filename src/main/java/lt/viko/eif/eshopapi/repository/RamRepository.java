d3603b556c9e35cb5a37e53855391e0c2fd92d2e
PC <lsongulija@gmail.com>
2021-06-07 18:35:07 +0200

    Ram model deleted

--- src/main/java/lt/viko/eif/eshopapi/repository/RamRepository.java
+++
@@ -1,7 +0,0 @@
-package lt.viko.eif.eshopapi.repository;
-
-import lt.viko.eif.eshopapi.model.Ram;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RamRepository extends CrudRepository<Ram, Long>{
-}
