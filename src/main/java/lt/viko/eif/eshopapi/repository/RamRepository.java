2027fac6f23ff4ab128a3ff1715fffb6b74e6dbe
PC <lsongulija@gmail.com>
2021-06-07 17:45:26 +0200

    Motherboard and RamRepository added

---
+++ src/main/java/lt/viko/eif/eshopapi/repository/RamRepository.java
@@ -0,0 +1,7 @@
+package lt.viko.eif.eshopapi.repository;
+
+import lt.viko.eif.eshopapi.model.Ram;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RamRepository extends CrudRepository<Ram, Long>{
+}
