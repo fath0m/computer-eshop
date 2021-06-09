1268d3703ca77f193292f9123a3dd2ee97141d69
Karalito <karl.pigaga@gmail.com>
2021-06-09 15:13:56 +0300

    Added Graphics card Service

--- src/main/java/lt/viko/eif/eshopapi/service/GraphicCardService.java
+++ src/main/java/lt/viko/eif/eshopapi/service/GraphicCardService.java
@@ -1,4 +1,41 @@
 package lt.viko.eif.eshopapi.service;
 
+import lt.viko.eif.eshopapi.dto.graphicCard.CreateGraphicCardDTO;
+import lt.viko.eif.eshopapi.dto.graphicCard.UpdateGraphicsCardDTO;
+import lt.viko.eif.eshopapi.model.GraphicsCard;
+import lt.viko.eif.eshopapi.model.Manufacturer;
+import lt.viko.eif.eshopapi.repository.GraphicsCardRepository;
+import lt.viko.eif.eshopapi.repository.ManufacturerRepository;
+import org.hibernate.graph.Graph;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class GraphicCardService {
+    @Autowired
+    GraphicsCardRepository graphicsCardRepository;
+    @Autowired
+    ManufacturerRepository manufacturerRepository;
+
+    private GraphicsCard buildFromDTO(CreateGraphicCardDTO dto){
+        GraphicsCard graphicsCard = new GraphicsCard();
+        graphicsCard.setName(dto.getName());
+
+        Optional<Manufacturer> manufacturer = manufacturerRepository.findById(dto.getManufacturer_id());
+        graphicsCard.setManufacturer(manufacturer.get());
+
+        return graphicsCard;
+    }
+
+    public GraphicsCard createGraphicsCard(CreateGraphicCardDTO createGraphicCardDTO){
+        GraphicsCard graphicsCard = buildFromDTO(createGraphicCardDTO);
+        return graphicsCardRepository.save(graphicsCard);
+    }
+
+    public GraphicsCard updateGraphicsCard(Long id, UpdateGraphicsCardDTO updateGraphicsCardDTO){
+        GraphicsCard graphicsCard = buildFromDTO(updateGraphicsCardDTO);
+        graphicsCard.setId(id);
+
+        return graphicsCardRepository.save(graphicsCard);
+    }
 }
