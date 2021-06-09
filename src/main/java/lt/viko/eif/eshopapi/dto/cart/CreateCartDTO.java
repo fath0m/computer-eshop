1ac7d0f9e3bb2fa1d756b05f5f320ebd322924bd
Karalito <karl.pigaga@gmail.com>
2021-06-09 14:34:36 +0300

    Added Payment and Cart DTO

--- src/main/java/lt/viko/eif/eshopapi/dto/cart/CreateCartDTO.java
+++ src/main/java/lt/viko/eif/eshopapi/dto/cart/CreateCartDTO.java
@@ -1,4 +1,9 @@
 package lt.viko.eif.eshopapi.dto.cart;
 
 public class CreateCartDTO {
+    private boolean isCheckedOut;
+
+    public boolean isCheckedOut() { return isCheckedOut; }
+
+    public void setCheckedOut(boolean checkedOut) { isCheckedOut = checkedOut; }
 }
--- src/main/java/lt/viko/eif/eshopapi/dto/payment/CreatePaymentDTO.java
+++ src/main/java/lt/viko/eif/eshopapi/dto/payment/CreatePaymentDTO.java
@@ -1,4 +1,22 @@
 package lt.viko.eif.eshopapi.dto.payment;
 
+import java.sql.Date;
+
 public class CreatePaymentDTO {
+
+    private Long checkOut_id;
+    private double amountPaid;
+    private Date paid_at;
+
+    public Long getCheckOut_id() { return checkOut_id; }
+
+    public void setCheckOut_id(Long checkOut_id) { this.checkOut_id = checkOut_id; }
+
+    public double getAmountPaid() { return amountPaid; }
+
+    public void setAmountPaid(double amountPaid) { this.amountPaid = amountPaid; }
+
+    public Date getPaid_at() { return paid_at; }
+
+    public void setPaid_at(Date paid_at) { this.paid_at = paid_at; }
 }
