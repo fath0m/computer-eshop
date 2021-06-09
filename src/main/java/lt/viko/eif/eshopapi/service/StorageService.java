39ade413f672eecb1da825e203ae14427f5b8892
PC <lsongulija@gmail.com>
2021-06-09 14:57:19 +0200

    Storage Service Added

--- src/main/java/lt/viko/eif/eshopapi/service/StorageService.java
+++ src/main/java/lt/viko/eif/eshopapi/service/StorageService.java
@@ -1,4 +1,39 @@
 package lt.viko.eif.eshopapi.service;
 
+import lt.viko.eif.eshopapi.dto.storage.CreateStorageDTO;
+import lt.viko.eif.eshopapi.model.Manufacturer;
+import lt.viko.eif.eshopapi.model.Storage;
+import lt.viko.eif.eshopapi.repository.ManufacturerRepository;
+import lt.viko.eif.eshopapi.repository.StorageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class StorageService {
+    @Autowired
+    StorageRepository storageRepository;
+    @Autowired
+    ManufacturerRepository manufacturerRepository;
+
+    private Storage buildFromDTO(CreateStorageDTO dto){
+        Storage storage = new Storage();
+        storage.setName(dto.getName());
+        storage.setAmount(dto.getAmount());
+
+        //Relationships
+        Optional<Manufacturer> manufacturer = manufacturerRepository.findById(dto.getManufacturerId());
+
+        storage.setManufacturer(manufacturer.get());
+
+        return storage;
+    }
+
+    public Storage createStorage(CreateStorageDTO createStorageDTO){
+        Storage storage = buildFromDTO(createStorageDTO);
+        return storageRepository.save(storage);
+    }
+
+    
+
+
 }
