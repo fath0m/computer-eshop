7818f3bf1b35a7bcdd1cfd4c73cfd12dbf4c3c82
PC <lsongulija@gmail.com>
2021-06-09 14:28:22 +0200

    Created Memory DTO

--- src/main/java/lt/viko/eif/eshopapi/dto/memory/CreateMemoryDTO.java
+++ src/main/java/lt/viko/eif/eshopapi/dto/memory/CreateMemoryDTO.java
@@ -1,4 +1,34 @@
 package lt.viko.eif.eshopapi.dto.memory;
 
+/**
+ * Creating Memory class DTO
+ */
 public class CreateMemoryDTO {
+    private String name;
+    private double amount;
+    private Long manufacturerId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Long getManufacturerId() {
+        return manufacturerId;
+    }
+
+    public void setManufacturerId(Long manufacturerId) {
+        this.manufacturerId = manufacturerId;
+    }
 }
