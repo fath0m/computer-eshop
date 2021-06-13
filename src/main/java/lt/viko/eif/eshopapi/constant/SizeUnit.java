bbd72005ac588f51d8b8ac99cdeaf01ad76439e3
Karalito <karl.pigaga@gmail.com>
2021-06-13 18:09:28 +0300

    Added Java docs for all Constants

--- src/main/java/lt/viko/eif/eshopapi/constant/SizeUnit.java
+++ src/main/java/lt/viko/eif/eshopapi/constant/SizeUnit.java
@@ -1,5 +1,11 @@
 package lt.viko.eif.eshopapi.constant;
 
+/**
+ * Represents Constants for SizeUnit
+ * This class is responsible for providing SizeUnits
+ * @author Karolis, Eimantas, Lukas, Jonas
+ * version 1.0
+ */
 public enum SizeUnit {
     KILOBYTE,
     MEGABYTE,
--- src/main/java/lt/viko/eif/eshopapi/constant/StorageType.java
+++ src/main/java/lt/viko/eif/eshopapi/constant/StorageType.java
@@ -1,5 +1,11 @@
 package lt.viko.eif.eshopapi.constant;
 
+/**
+ * Represents Constants for StorageType
+ * This class is responsible for providing StorageTypes
+ * @author Karolis, Eimantas, Lukas, Jonas
+ * version 1.0
+ */
 public enum StorageType {
     HDD,
     SATA_SSD,
