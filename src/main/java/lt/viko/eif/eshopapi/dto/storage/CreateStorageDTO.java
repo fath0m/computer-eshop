85157714cd6b57819b7abd58a55c9975a728c927
Eimis86 <75447681+Eimis86@users.noreply.github.com>
2021-06-13 18:22:30 +0300

    comments dto processor, storage

--- src/main/java/lt/viko/eif/eshopapi/dto/storage/CreateStorageDTO.java
+++ src/main/java/lt/viko/eif/eshopapi/dto/storage/CreateStorageDTO.java
@@ -1,5 +1,11 @@
 package lt.viko.eif.eshopapi.dto.storage;
 
+/**
+ * Contains database variables and foreing keys
+ * and contains those variable getters and setters
+ * @author Lukas, Eimantas, Jonas, Karolis
+ * version 1.0
+ */
 public class CreateStorageDTO {
     private String name;
     private double amount;
