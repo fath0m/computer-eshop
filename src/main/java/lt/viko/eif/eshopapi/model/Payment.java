1124b050816d532be02c29f4fb39f6a524efba4a
PC <lsongulija@gmail.com>
2021-06-13 17:54:18 +0200

    Added javadoc to Payment object

--- src/main/java/lt/viko/eif/eshopapi/model/Payment.java
+++ src/main/java/lt/viko/eif/eshopapi/model/Payment.java
@@ -5,6 +5,12 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import javax.persistence.*;
 import java.sql.Date;
 
+/**
+ * Represents Payment object which has ManyToOne connections
+ * with other Tables Checkout.
+ * This class is responsible for getting, setting properties
+ * @author Lukas, Eimantas, Jonas, Karolis
+ */
 @Entity
 @Table(name = "payments")
 public class Payment {
@@ -12,7 +18,9 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    /**
+     * Setting OneToMany connection with checkout
+     */
     @JsonManagedReference
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "checkout_id", nullable = false)
@@ -21,34 +29,66 @@ public class Payment {
     private double amountPaid;
     private Date paidAt;
 
+    /**
+     * Getter for id field
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Setter for id field
+     * @param id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Getter for checkout object
+     * @return checkout
+     */
     public Checkout getCheckout() {
         return checkout;
     }
 
+    /**
+     * Setter for checkout object
+     * @param checkout
+     */
     public void setCheckout(Checkout checkout) {
         this.checkout = checkout;
     }
 
+    /**
+     * Getter for amountPaid field
+     * @return amountPaid
+     */
     public double getAmountPaid() {
         return amountPaid;
     }
 
+    /**
+     * Setter for amountPaid field
+     * @param amountPaid
+     */
     public void setAmountPaid(double amountPaid) {
         this.amountPaid = amountPaid;
     }
 
+    /**
+     * Getter for paidAt field
+     * @return paidAt
+     */
     public Date getPaidAt() {
         return paidAt;
     }
 
+    /**
+     * Setter for paidAt field
+     * @param paidAt
+     */
     public void setPaidAt(Date paidAt) {
         this.paidAt = paidAt;
     }
