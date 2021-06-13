d96ebdf5b4401a3e2256151fa04dd6e74ec482dc
Jonas Ritchi <ritchijon@gmail.com>
2021-06-13 21:14:36 +0300

    Added cart items unit tests

---
+++ src/test/java/lt/viko/eif/eshopapi/dto/cartItems/CreateCartItemsDTOTest.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.viko.eif.eshopapi.dto.cartItems;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jonas
+ */
+public class CreateCartItemsDTOTest {
+    
+    public CreateCartItemsDTOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getComputerId method, of class CreateCartItemsDTO.
+     */
+    @Test
+    public void testGetComputerId() {
+        System.out.println("getComputerId");
+        CreateCartItemsDTO instance = new CreateCartItemsDTO();
+        Long expResult = null;
+        Long result = instance.getComputerId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setComputerId method, of class CreateCartItemsDTO.
+     */
+    @Test
+    public void testSetComputerId() {
+        System.out.println("setComputerId");
+        Long computerId = null;
+        CreateCartItemsDTO instance = new CreateCartItemsDTO();
+        instance.setComputerId(computerId);
+        assertEquals(computerId, instance.getComputerId());
+    }
+
+    /**
+     * Test of getCartId method, of class CreateCartItemsDTO.
+     */
+    @Test
+    public void testGetCartId() {
+        System.out.println("getCartId");
+        CreateCartItemsDTO instance = new CreateCartItemsDTO();
+        Long expResult = null;
+        Long result = instance.getCartId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setCartId method, of class CreateCartItemsDTO.
+     */
+    @Test
+    public void testSetCartId() {
+        System.out.println("setCartId");
+        Long cartId = null;
+        CreateCartItemsDTO instance = new CreateCartItemsDTO();
+        instance.setCartId(cartId);
+        assertEquals(cartId, instance.getCartId());
+    }
+
+    /**
+     * Test of getQuantity method, of class CreateCartItemsDTO.
+     */
+    @Test
+    public void testGetQuantity() {
+        System.out.println("getQuantity");
+        CreateCartItemsDTO instance = new CreateCartItemsDTO();
+        Long expResult = null;
+        Long result = instance.getQuantity();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setQuantity method, of class CreateCartItemsDTO.
+     */
+    @Test
+    public void testSetQuantity() {
+        System.out.println("setQuantity");
+        Long quantity = null;
+        CreateCartItemsDTO instance = new CreateCartItemsDTO();
+        instance.setQuantity(quantity);
+        assertEquals(quantity, instance.getQuantity());
+    }
+    
+}
