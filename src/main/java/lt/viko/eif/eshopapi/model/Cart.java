15d03f69d5944763ebae642bc2698020a7cd325e
Karalito <karl.pigaga@gmail.com>
2021-06-13 17:47:41 +0300

    Added cart Java docs

--- src/main/java/lt/viko/eif/eshopapi/model/Cart.java
+++ src/main/java/lt/viko/eif/eshopapi/model/Cart.java
@@ -2,6 +2,12 @@ package lt.viko.eif.eshopapi.model;
 
 import javax.persistence.*;
 
+/**
+ * Represents Cart object which has id, isCheckedOut properties
+ * This class is responsible for getting, setting above mentioned properties
+ * @author Karolis, Eimantas, Lukas, Jonas
+ * version 1.0
+ */
 @Entity
 @Table(name = "carts")
 public class Cart {
@@ -11,11 +17,27 @@ public class Cart {
     private Long id;
     private boolean isCheckedOut;
 
+    /**
+     * Getter for cart Id
+     * @return id
+     */
     public Long getId() { return id; }
 
+    /**
+     * Setter for cart Id
+     * @param id
+     */
     public void setId(Long id) { this.id = id; }
 
+    /**
+     * Getter for is item checked out
+     * @return isCheckedOut (true or false)
+     */
     public boolean isCheckedOut() { return isCheckedOut; }
 
+    /**
+     * Setter for checkedOut
+     * @param checkedOut
+     */
     public void setCheckedOut(boolean checkedOut) { isCheckedOut = checkedOut; }
 }
